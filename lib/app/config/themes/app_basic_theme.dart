4858cd83a7c59568648f89b4ea60faa91c5161c1
giadeveloper <giagamedeveloper@gmail.com>
2021-05-16 22:21:51 +0700

    add design form login

--- lib/app/config/themes/app_basic_theme.dart
+++ lib/app/config/themes/app_basic_theme.dart
@@ -4,6 +4,30 @@ abstract class AppBasicTheme {
   static ThemeData getThemeData() {
     return ThemeData(
       primarySwatch: Colors.blue,
+      textTheme: TextTheme(bodyText2: TextStyle(color: Colors.grey[800])),
+      inputDecorationTheme: InputDecorationTheme(
+        filled: true,
+        fillColor: Colors.grey[200],
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          minimumSize: Size(double.infinity, 55),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(35),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/app/features/login/views/components/login_button.dart
@@ -0,0 +1,10 @@
+part of login_view;
+
+class _LoginButton extends StatelessWidget {
+  const _LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: () {}, child: Text("Login"));
+  }
+}
